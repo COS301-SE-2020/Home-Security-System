0f522117850c44f0ab2f5c477f60a35421e99c28
Bradford-700 <u15228194@tuks.co.za>
2020-06-09 15:48:31 +0200

    links

--- argus-app/src/app/app-routing.module.ts
+++ argus-app/src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { NotificationsComponent } from './notifications/notifications.component';
-import { ListPeopleComponent } from './Person/list-people/list-people.component';
-import { ListVehiclesComponent } from './Vehicle/list-vehicles/list-vehicles.component';
-import { ListUsersComponent } from './User/list-users/list-users.component';
-
-/*when you want to view the various pages just make the path: ''*/
-const routes: Routes = [
-  {path: 'a', component: NotificationsComponent},
-  {path: '', component: ListPeopleComponent},
-  {path: 'c', component: ListVehiclesComponent},
-  {path: 'd', component: ListUsersComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- argus-app/src/app/app.module.ts
+++ argus-app/src/app/app.module.ts
@@ -9,15 +9,22 @@ import { NotificationsComponent } from './notifications/notifications.component'
 import { ListPeopleComponent } from './Person/list-people/list-people.component';
 import { ListVehiclesComponent } from './Vehicle/list-vehicles/list-vehicles.component';
 import { ListUsersComponent } from './User/list-users/list-users.component';
+import { AddPersonComponent } from './Person/add-person/add-person.component';
+import { UpdatePersonComponent } from './Person/update-person/update-person.component';
+import { AddVehicleComponent } from './Vehicle/add-vehicle/add-vehicle.component';
+import { UpdateVehicleComponent } from './Vehicle/update-vehicle/update-vehicle.component';
+
 
 //added
 const appRoutes: Routes =[
-
-{path: 'notifications', component: NotificationsComponent },
-{path: 'people', component: ListPeopleComponent },
-{path: 'vehicle', component: ListVehiclesComponent },
-{path: 'users', component: ListUsersComponent }
-
+  {path: 'notifications', component: NotificationsComponent },
+  {path: 'people', component: ListPeopleComponent },
+  {path: 'vehicle', component: ListVehiclesComponent },
+  {path: 'users', component: ListUsersComponent },
+  {path: 'addp', component: AddPersonComponent },
+  {path: 'addv', component: AddVehicleComponent },
+  {path: 'editp', component: UpdatePersonComponent },
+  {path: 'editv', component: UpdateVehicleComponent }
 ];
 
 @NgModule({
