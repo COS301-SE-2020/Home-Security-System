70939faa9dcd32a0a17e685664c385b144f9c7d5
Brett Fourie <brett4e@gmail.com>
2020-07-10 11:24:11 +0200

    Separated local and email notifications in RabbitMQ.

--- Argus/Angular-Frontend/src/assets/js/Rabbit/receive.js
+++ Argus/Angular-Frontend/src/assets/js/Rabbit/receive.js
@@ -1,41 +1,51 @@
 #!/usr/bin/env node
+var amqp = require('amqplib/callback_api');
+var msgsArr = [];
+var localArr = [];
+var emailArr = [];
 
-
-  var amqp = require('amqplib/callback_api');
-  var msgsArr = [];
-
-  amqp.connect('amqp://localhost', function (error0, connection) {
-    if (error0) {
-      throw error0;
+amqp.connect('amqp://localhost', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
     }
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
 
-      var queue = 'notifications';
+    var queue = 'notifications';
 
-      channel.assertQueue(queue, {
-        durable: false
-      });
+    channel.assertQueue(queue, {
+      durable: false
+    });
 
-      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-        channel.consume(queue, function (msg) {
-          msgsArr.push(msg.content.toString());
-          const fs = require('fs');
+    channel.consume(queue, function (msg) {
 
-          let msgs = {
-            message: msgsArr
-          };
-          let data = JSON.stringify(msgs);
-          fs.writeFileSync('messages.json', data);
+      msgsArr.push(msg.content.toString());
 
-          console.log(" [x] Received %s", msg.content.toString());
-      }, {
-        noAck: true
-      });
+      if (msg.content.toString().substr(0, 1) === "L")
+        localArr.push(msg.content.toString());
+
+      else
+        emailArr.push(msg.content.toString());
+
+      const fs = require('fs');
+
+      let msgs = {
+        message: msgsArr,
+        local: localArr,
+        email: emailArr
+      };
+      let data = JSON.stringify(msgs);
+      fs.writeFileSync('messages.json', data);
+
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {
+      noAck: true
     });
   });
+});
 
 
--- Argus/Angular-Frontend/src/assets/js/Rabbit/sendNo2.js
+++ Argus/Angular-Frontend/src/assets/js/Rabbit/sendNo2.js
@@ -1,29 +1,31 @@
 #!/usr/bin/env node
-    var amqp = require('amqplib/callback_api');
+var amqp = require('amqplib/callback_api');
 
-    amqp.connect('amqp://localhost', function(error0, connection) {
-        if (error0) {
-            throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
+amqp.connect('amqp://localhost', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
 
-            var queue = 'notifications';
-            var msg = 'Testing the email system';
+    var queue = 'notifications';
+    var msg = 'Email message sent';
 
-            channel.assertQueue(queue, {
-                durable: false
-            });
-            channel.sendToQueue(queue, Buffer.from(msg));
-
-            console.log(" [x] Sent %s", msg);
-        });
-        setTimeout(function() {
-            connection.close();
-            process.exit(0);
-        }, 500);
+    channel.assertQueue(queue, {
+      durable: false
     });
+
+    channel.sendToQueue(queue, Buffer.from(msg));
+
+    console.log(" [x] Sent %s", msg);
+  });
+
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
 
 
