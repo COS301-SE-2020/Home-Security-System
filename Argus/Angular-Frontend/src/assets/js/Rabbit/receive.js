8063b6403d97a21d3ab21899ed354a64cc86d834
Brett Fourie <brett4e@gmail.com>
2020-07-10 10:32:21 +0200

    Added login and sidebar functionality re-upload.

--- Argus/Angular-Frontend/src/assets/js/Rabbit/receive.js
+++ Argus/Angular-Frontend/src/assets/js/Rabbit/receive.js
@@ -1,41 +1,41 @@
 #!/usr/bin/env node
-/*
-            channel.consume(queue, function(msg) {
-                console.log(" [x] Received %s", msg.content.toString());
-            const fs = require('fs');
 
-            let student = {
-                local: ["1"],
-                email: ["2"]
-            };
-            let data = JSON.stringify(student);
-            fs.writeFileSync('messages.json', data);
-            */
-    var amqp = require('amqplib/callback_api');
 
-    amqp.connect('amqp://localhost', function(error0, connection) {
-        if (error0) {
-            throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
+  var amqp = require('amqplib/callback_api');
+  var msgsArr = [];
 
-            var queue = 'notifications';
+  amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
 
-            channel.assertQueue(queue, {
-                durable: false
-            });
+      var queue = 'notifications';
 
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+      channel.assertQueue(queue, {
+        durable: false
+      });
 
-            channel.consume(queue, function(msg) {
-                console.log(" [x] Received %s", msg.content.toString());
-            }, {
-                noAck: true
-            });
-        });
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function (msg) {
+          msgsArr.push(msg.content.toString());
+          const fs = require('fs');
+
+          let msgs = {
+            message: msgsArr
+          };
+          let data = JSON.stringify(msgs);
+          fs.writeFileSync('messages.json', data);
+
+          console.log(" [x] Received %s", msg.content.toString());
+      }, {
+        noAck: true
+      });
     });
+  });
 
 
