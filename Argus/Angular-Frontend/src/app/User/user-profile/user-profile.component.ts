59b69eb0a26fbd6c7e6ae9d84040d919add00115
JohannSS <u16008554@tuks.co.za>
2020-07-19 00:52:25 +0200

    Added some CRUD operations to the User-profile component

--- Argus/Angular-Frontend/src/app/User/user-profile/user-profile.component.ts
+++ Argus/Angular-Frontend/src/app/User/user-profile/user-profile.component.ts
@@ -1,3 +1,5 @@
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+
 import { Component, OnInit } from '@angular/core';
 import {TitleService} from '../../title.service';
 
@@ -6,9 +8,79 @@ import {TitleService} from '../../title.service';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit
+{
+  public usersList: AngularFireList<any>;
 
-  constructor(private appService: TitleService) { }
+  constructor(private appService: TitleService, private db: AngularFireDatabase) {
+    this.usersList = db.list('/users');
+  }
+
+  /* ======================================================== */
+
+
+  ReadDB(): void {
+    let uL = this.db.database.ref('users');
+    let obj = null;
+    uL.orderByValue().on('value', function (snapshot): void {
+      snapshot.forEach(function (data): void {
+        if (data.val().name === 'Johann') {
+          obj = data.val();
+        }
+      });
+      if (obj === null) {
+        console.log('error');
+      } else {
+        let userName = document.getElementById('firstName') as HTMLDataElement;
+        userName.value = obj.name;
+
+        let userSurname = document.getElementById('lastName') as HTMLDataElement;
+        userSurname.value = obj.surname;
+
+        let userEmail = document.getElementById('email') as HTMLDataElement;
+        userEmail.value = obj.email;
+
+        let userUsername = document.getElementById('username') as HTMLDataElement;
+        userUsername.value = obj.username;
+
+
+
+        let userNameDisplay = document.getElementById('firstNameDisplay') as HTMLDataElement;
+        userNameDisplay.value = obj.name;
+
+        let userSurnameDisplay = document.getElementById('lastNameDisplay') as HTMLDataElement;
+        userSurnameDisplay.value = obj.surname;
+
+        let userEmailDisplay = document.getElementById('emailDisplay') as HTMLDataElement;
+        userEmailDisplay.value = obj.email;
+
+        let userUsernameDisplay = document.getElementById('usernameDisplay') as HTMLDataElement;
+        userUsernameDisplay.value = obj.username;
+
+        // console.log(userName);
+        // console.log(userSurname);
+        // console.log(userUsername);
+        // console.log(userEmail);
+
+      }
+    });
+  }
+
+  public AddDB(): void {
+    const user = {
+      name : 'Johann',
+      surname : 'Stadler',
+      username: 'Stads',
+      email: 'sigmacos301@gmail.com',
+      role: 'Advanced',
+      password: '1234password',
+      profilePicture: ''
+    };
+    this.usersList.push(user);
+
+  }
+
+  /* ======================================================== */
 
   ngOnInit(): void {
     this.appService.setTitle('User Profile');
