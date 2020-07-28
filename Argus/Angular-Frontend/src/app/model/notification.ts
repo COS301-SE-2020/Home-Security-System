122aad21ffc7a7fd1825f8ee7b8a5d03d8db0c81
Bradford-700 <u15228194@tuks.co.za>
2020-07-28 23:23:19 +0200

    lol

--- Argus/Angular-Frontend/src/app/model/notification.ts
+++ Argus/Angular-Frontend/src/app/model/notification.ts
@@ -3,6 +3,7 @@ import {Image} from './image';
 export interface Notification {
   id: number;
   notificationImg: Image;
+  user_id: number;
   message: string;
   onDate: string;
   atTime: string;
