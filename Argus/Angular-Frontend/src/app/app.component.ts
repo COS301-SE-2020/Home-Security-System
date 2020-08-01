a235d799a924ae13f5aa8553ece4ed65fd6558b6
JohannSS <u16008554@tuks.co.za>
2020-08-01 15:01:43 +0200

    Made the side_nav and top_nav hide on login and appear everywhere else

--- Argus/Angular-Frontend/src/app/app.component.ts
+++ Argus/Angular-Frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 import { TitleService } from './title.service';
+import {LoginComponent} from './Dashboard/login/login.component';
 
 
 @Component({
@@ -21,7 +22,10 @@ export class AppComponent implements OnInit {
     this.titleService.setTitle(title);
   }
 
-
+  isLoginPage(): boolean {
+    const child = this.activatedRoute.firstChild;
+    return child.component === LoginComponent;
+  }
 
 
   ngOnInit() {
@@ -31,6 +35,23 @@ export class AppComponent implements OnInit {
       filter(event => event instanceof NavigationEnd),
       map(() => {
         let child = this.activatedRoute.firstChild;
+
+        console.log(this.isLoginPage());
+        if ( this.isLoginPage() === true)
+        {
+          document.getElementById('topNav').style.visibility = 'hidden';
+          document.getElementById('sideNav').style.visibility = 'hidden';
+          // document.getElementById('topNav').remove();
+          // document.getElementById('sideNav').remove();
+        }
+        else
+        {
+          document.getElementById('topNav').style.visibility = 'visible';
+          document.getElementById('sideNav').style.visibility = 'visible';
+          // document.getElementById('topNav').innerHTML = '<app-top-nav></app-top-nav>';
+          // document.getElementById('sideNav').innerHTML = ' <app-side-nav></app-side-nav>';
+        }
+
         while (child.firstChild) {
           child = child.firstChild;
         }
