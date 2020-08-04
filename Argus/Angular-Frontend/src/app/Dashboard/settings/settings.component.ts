510dc8071b5294b55f9b470296b491beb60fba0c
ShaunVermeulen <u16078625@tuks.co.za>
2020-08-04 19:23:22 +0200

    Settings styling refined. Added feature for saving settings to the user's data. Need to check updateUser function as it is not currently working

--- Argus/Angular-Frontend/src/app/Dashboard/settings/settings.component.ts
+++ Argus/Angular-Frontend/src/app/Dashboard/settings/settings.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {TitleService} from '../../title.service';
+import { Observable } from 'rxjs';
+import { UserService } from '../../model/user.service';
+import { User } from '../../model/user';
+import { Session } from '../../../assets/js/SessionStorage.js';
 
 @Component({
   selector: 'app-settings',
@@ -7,15 +11,59 @@ import {TitleService} from '../../title.service';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  sessionS = new Session();
+  users: Observable<User[]>;
+  user: User;
 
-  constructor(private appService: TitleService) {}
+  constructor(private appService: TitleService, private userService: UserService) {}
 
-  ReadDB(): void {}
+  enableButton(){
+    const buttonEl = document.getElementById('saveBtn') as HTMLButtonElement;
+    buttonEl.style.background = '#d4af37';
+    buttonEl.disabled = false;
+  }
 
-  UpdateDB(): void {}
+  retrieveSettings(){
+    const buttonEl = document.getElementById('saveBtn') as HTMLButtonElement;
+    const localSettings = document.getElementById('localSlider') as HTMLInputElement;
+    const emailSettings = document.getElementById('emailSlider') as HTMLInputElement;
+
+    buttonEl.style.background = 'grey';
+    buttonEl.disabled = true;
+
+    let userObj;
+    userObj = this.sessionS.retrieveUserInfo();
+    /*this.users = */
+    this.userService.getUserById(userObj.id).subscribe(
+      data => {
+        localSettings.checked = data.notifyLocal;
+        emailSettings.checked = data.notifyEmail;
+        this.user = data;
+        // console.log(this.user);
+      }
+    );
+  }
+
+  setUserSettings(){
+    // this.user = new User();
+    const localSet = document.getElementById('localSlider') as HTMLInputElement;
+    const emailSet = document.getElementById('emailSlider') as HTMLInputElement;
+    let userObj;
+    userObj = this.sessionS.retrieveUserInfo();
+    this.user.notifyEmail = emailSet.checked;
+    this.user.notifyLocal = localSet.checked;
+
+    console.log(this.user.notifyEmail);
+    console.log(this.user.notifyLocal);
+
+    // console.log(this.user);
+    this.userService.updateUser(1, this.user).subscribe(data => console.log(data), error => console.log(error));
+    this.user = new User();
+    this.retrieveSettings();
+  }
 
   ngOnInit(): void {
     this.appService.setTitle('Settings');
-    this.ReadDB();
+    this.retrieveSettings();
   }
 }
