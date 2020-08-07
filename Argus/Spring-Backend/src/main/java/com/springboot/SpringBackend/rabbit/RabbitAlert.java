618a2771cb2c98dd260fa9342ad64a00f4d3be69
Bradford-700 <u15228194@tuks.co.za>
2020-08-07 16:35:50 +0200

    RabbitMQ Producer & Consumer for Alerts/New Grey list people

---
+++ Argus/Spring-Backend/src/main/java/com/springboot/SpringBackend/rabbit/RabbitAlert.java
@@ -0,0 +1,27 @@
+package com.springboot.SpringBackend.rabbit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RabbitAlert implements Serializable {
+    private static final long serialVersionUID = -7446583075938108797L;
+
+    private String image;
+    private int pid;
+
+    public RabbitAlert() {}
+
+    public RabbitAlert(@JsonProperty("imageStr") String img, @JsonProperty("id") int id) {
+        this.image = img;
+        this.pid = id;
+    }
+
+    public String getImage() { return this.image; }
+    public void setImage(String img) { this.image = img; }
+
+    public int getPid() { return this.pid; }
+    public void setPid(int id) { this.pid = id; }
+}
--- Argus/Spring-Backend/src/main/java/com/springboot/SpringBackend/rabbit/RabbitConsumer.java
+++ Argus/Spring-Backend/src/main/java/com/springboot/SpringBackend/rabbit/RabbitConsumer.java
@@ -1,18 +1,49 @@
 package com.springboot.SpringBackend.rabbit;
 
-import com.springboot.SpringBackend.SpringBackendApplication;
+import com.springboot.SpringBackend.model.*;
+import com.springboot.SpringBackend.service.NotificationService;
+import com.springboot.SpringBackend.service.PersonService;
+import com.springboot.SpringBackend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.Message;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-//@Service
+import java.util.Optional;
+
+@Component
 public class RabbitConsumer {
-   // private static final Logger log = LoggerFactory.getLogger(RabbitConsumer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitConsumer.class);
 
-    //@RabbitListener(queues = SpringBackendApplication.DEFAULT_PARSING_QUEUE)
-    //public void consumeDefaultMessage(final Message message) {
-    //    log.info("Received message, tip is: {}", message.toString());
-    //}
+    private final NotificationService nservice;
+    private final PersonService pservice;
+    private final UserService uservice;
+
+    @Autowired
+    public RabbitConsumer(NotificationService ns, PersonService ps, UserService us) {
+        this.nservice = ns;
+        this.pservice = ps;
+        this.uservice = us;
+    }
+
+    @RabbitListener(queues = {"alertQueue"})
+    public void receivedAlert(RabbitAlert alert) {
+        Long num = Long.valueOf(1);
+        Optional<User> u =  uservice.getUserById(num);
+        Optional<Person> x =  pservice.getPersonById(Long.valueOf(alert.getPid()));
+        nservice.createNotification(new Notification(new Image(alert.getImage()),
+                "Intruder" + x.get().getFname() + " " + x.get().getLname(), u.get()));
+        LOGGER.info("Alert Received");
+    }
+
+
+    /*
+    @RabbitListener(queues = {"persontQueue"})
+    public void receivedPerson(RabbitPerson newperson) {
+        pservice.createPerson(new Person(new Image(newperson.getImage()), "Unknown", "Unknown");
+        LOGGER.info("Person Created");
+    }
+    */
+
 }
