64bbca184760c2767d1709da62fa33054d245bdf
JohannSS <johannss29@gmail.com>
2020-08-30 00:06:40 +0200

    Got the bargraph working

--- Argus/Angular-Frontend/src/app/Dashboard/dashboard/dashboard.component.ts
+++ Argus/Angular-Frontend/src/app/Dashboard/dashboard/dashboard.component.ts
@@ -4,7 +4,10 @@ import {Observable, Subject} from 'rxjs';
 import {TitleService} from '../../title.service';
 import {PersonService} from '../../model/person.service';
 import {Person} from '../../model/person';
+import {UserService} from '../../model/user.service';
 import * as CanvasJS from '../../../assets/js/canvasjs.min';
+import {Notification} from '../../model/notification';
+import {NotificationService} from '../../model/notification.service';
 // import {getLocaleDateFormat} from '@angular/common';
 // import {UserService} from '../../model/user.service';
 // import {User} from '../../model/user';
@@ -16,10 +19,13 @@ import * as CanvasJS from '../../../assets/js/canvasjs.min';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private appService: TitleService, private personService: PersonService) {}
+  constructor(private appService: TitleService, private personService: PersonService, private notificationService: NotificationService) {}
 
   people: Observable<Person[]>;
   currentDate = new Date();
+
+  notification: Observable<Notification[]>;
+  note: Notification;
 
   // ----------------------
   title = 'argus-app';
@@ -46,6 +52,8 @@ export class DashboardComponent implements OnInit {
     this.camImg = img;
   }
 
+  // ==========================================================================================
+
   public calculateNumberOfPeople(): void{
     this.personService.getPersonList().subscribe(data => {
       let threat = 0;
@@ -66,7 +74,130 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  public barchart(): void{
+  // ==========================================================================================
+
+  public calculateNumberOfNotifications(): void{
+    this.notificationService.getNotificationList().subscribe(data => {
+
+      this.note = new Notification();
+      this.notification = this.notificationService.getNotificationList();
+      // this.notification;
+      // console.log(data[0].onDate);
+      // console.log( this.dayTester(0));
+
+      let six = 0;
+      let five = 0;
+      let four = 0;
+      let three = 0;
+      let two = 0;
+      let one = 0;
+      let today = 0;
+
+      for (let i = 0; i < data.length; i++){
+        if (data[i].onDate === this.dayTester(0)){
+          today++;
+        }
+        else if (data[i].onDate === this.dayTester(1)){
+          one++;
+        }
+        else if (data[i].onDate === this.dayTester(2)){
+          two++;
+        }
+        else if (data[i].onDate === this.dayTester(3)){
+          three++;
+        }
+        else if (data[i].onDate === this.dayTester(4)){
+          four++;
+        }
+        else if (data[i].onDate === this.dayTester(5)){
+          five++;
+        }
+        else if (data[i].onDate === this.dayTester(6)){
+          six++;
+        }
+        else{
+        }
+      }
+
+
+      /*
+      for (let i = 0; i < data.length; i++){
+        if (data[i].personListed === 'Grey'){
+          suspicious++;
+        }
+        else if (data[i].personListed === 'Black'){
+          threat++;
+        }
+        else{
+          cleared++;
+        }
+      }
+       */
+      this.barchart(six, five, four, three, two, one, today );
+    });
+  }
+
+  // ==========================================================================================
+
+  public dayTester( days ) {
+    const date = new Date();
+    const last = new Date(date.getTime() - (days * 24 * 60 * 60 * 1000));
+    const day = last.getDate();
+    const month = 1 + last.getMonth();
+    let monthSmall = '';
+    let daySmall = '';
+
+    if ( month < 10 )
+    {
+      monthSmall = ('0' + month).toString();
+    }
+    if ( day < 10 )
+    {
+      daySmall = ('0' + month).toString();
+    }
+    const year = last.getFullYear();
+
+    if ( monthSmall === '' && daySmall === '')
+    {
+      return (year + '-' + month + '-' + day).toString();
+    }
+    else if (monthSmall === '' && daySmall !== '')
+    {
+      return (year + '-' + month + '-' + daySmall).toString();
+    }
+    else if ( monthSmall !== '' && daySmall === '')
+    {
+      return (year + '-' + monthSmall + '-' + day).toString();
+    }
+    else if (monthSmall !== '' && daySmall !== '')
+    {
+      return (year + '-' + monthSmall + '-' + daySmall).toString();
+    }
+    else
+    {
+      return (year + '-' + month + '-' + day).toString();
+    }
+  }
+
+  // ==========================================================================================
+
+  public getCorrectDay( days ) {
+    const date = new Date();
+    const last = new Date(date.getTime() - (days * 24 * 60 * 60 * 1000));
+    const day = last.getDate();
+    const month = 1 + last.getMonth();
+    // const year = last.getFullYear();
+    return (day + '/' + month).toString();
+  }
+
+  // ==========================================================================================
+
+  public barchart(Mon, Tue, Wed, Thu, Fri, Sat, Sun): void{
+    const date = new Date();
+
+    const days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let dayName = days[date.getDay()];
+
     const chart = new CanvasJS.Chart('chartContainer', {
       animationEnabled: true,
       exportEnabled: true,
@@ -76,19 +207,31 @@ export class DashboardComponent implements OnInit {
       data: [{
         type: 'column',
         dataPoints: [
-          { y: 5, label: 'Monday' },
-          { y: 4, label: 'Tuesday' },
-          { y: 3, label: 'Wednesday' },
-          { y: 2, label: 'Thursday' },
-          { y: 0, label: 'Friday' },
-          { y: 0, label: 'Saturday' },
-          { y: 0, label: 'Sunday' }
+          /*
+          { y: Mon, label: days[-6 + date.getDay()]},
+          { y: Tue, label: days[-5 + date.getDay()]},
+          { y: Wed, label: days[-4 + date.getDay()]},
+          { y: Thu, label: days[-3 + date.getDay()]},
+          { y: Fri, label: days[-2 + date.getDay()] },
+          { y: Sat, label: days[-1 + date.getDay()] },
+          { y: Sun, label: days[date.getDay()]}
+           */
+          { y: Mon, label: this.getCorrectDay(6)},
+          { y: Tue, label: this.getCorrectDay(5)},
+          { y: Wed, label: this.getCorrectDay(4)},
+          { y: Thu, label: this.getCorrectDay(3)},
+          { y: Fri, label: this.getCorrectDay(2)},
+          { y: Sat, label: this.getCorrectDay(1)},
+          { y: Sun, label: this.getCorrectDay(0)}
         ]
       }]
     });
 
     chart.render();
   }
+
+  // ==========================================================================================
+
   public pieChart(val1, val2, val3): void{
     const chart = new CanvasJS.Chart('chartContainer2', {
       // light2 or dark2
@@ -114,14 +257,15 @@ export class DashboardComponent implements OnInit {
     chart.render();
   }
 
-
+// ==========================================================================================
   // private ChartItems: ChartService;
 
   ngOnInit(): void {
     this.appService.setTitle('Dashboard');
     // this.getChartDetails();
-    this.barchart();
+    // this.barchart();
     this.calculateNumberOfPeople();
+    this.calculateNumberOfNotifications();
     // this.pieChart();
   }
 
