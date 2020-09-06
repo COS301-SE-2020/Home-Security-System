304ba27fbbb5210ec21a465a3ee70aee18093489
ShaunVermeulen <u16078625@tuks.co.za>
2020-09-06 23:21:28 +0200

    More unit tests added to the following components: -dashboard component  ->adapted the dashboard html to work with unit test -notification component  ->adapted the notification html to work with unit test -settings component -top nav component -side nav component  ->adapted the side-nav html to work with unit test
     The notifications headers now all have uniform font types

--- Argus/Angular-Frontend/src/app/Dashboard/dashboard/dashboard.component.spec.ts
+++ Argus/Angular-Frontend/src/app/Dashboard/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard.component';
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +10,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      imports: [HttpClientModule, RouterModule.forRoot([])]
     })
     .compileComponents();
   }));
@@ -18,6 +21,29 @@ describe('DashboardComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  it('live feed recorder display', () => {
+    // tslint:disable-next-line:no-shadowed-variable
+    const fixture = TestBed.createComponent(DashboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#icon1').textContent).toContain('stop_circle');
+  });
+  it('notifications graph should display updated value date', () => {
+    // tslint:disable-next-line:no-shadowed-variable
+    const fixture = TestBed.createComponent(DashboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#access1').textContent).toContain('access_time');
+  });
+  it('people graph should display updated value date', () => {
+    // tslint:disable-next-line:no-shadowed-variable
+    const fixture = TestBed.createComponent(DashboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#access2').textContent).toContain('access_time');
+  });
+
 
   /*it('should create', () => {
     expect(component).toBeTruthy();
--- Argus/Angular-Frontend/src/app/Dashboard/side-nav/side-nav.component.spec.ts
+++ Argus/Angular-Frontend/src/app/Dashboard/side-nav/side-nav.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { SideNavComponent } from './side-nav.component';
 
 describe('SideNavComponent', () => {
@@ -8,7 +9,8 @@ describe('SideNavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SideNavComponent ]
+      declarations: [ SideNavComponent ],
+      imports: [HttpClientModule, RouterModule.forRoot([])]
     })
     .compileComponents();
   }));
@@ -21,5 +23,16 @@ describe('SideNavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display the side-nav options correctly', () => {
+    // tslint:disable-next-line:no-shadowed-variable
+    const fixture = TestBed.createComponent(SideNavComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#dashLink').textContent).toContain('Dashboard');
+    expect(compiled.querySelector('#userLink').textContent).toContain('User List');
+    expect(compiled.querySelector('#clearedLink').textContent).toContain('Cleared List');
+    expect(compiled.querySelector('#unknownLink').textContent).toContain('Unknown List');
+    expect(compiled.querySelector('#threatLink').textContent).toContain('Threat List');
   });
 });
