38286afee6dfc7a965944e5928047b3f3f5210c1
Bradford-700 <u15228194@tuks.co.za>
2020-09-11 04:55:47 +0200

    Heroku configuration frontend and backend

--- Argus/Angular-Frontend/src/app/model/image.service.ts
+++ Argus/Angular-Frontend/src/app/model/image.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import {environment} from '../../environments/environment';
+// import {environment} from '../../environments/environment.prod';
+
 @Injectable({
   providedIn: 'root'
 })
