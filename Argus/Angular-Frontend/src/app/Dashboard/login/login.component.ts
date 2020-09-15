d323e71d5845b26cbf1ee67b1deaf113fd80feb5
Brett Fourie <brett4e@gmail.com>
2020-09-15 16:33:25 +0200

    Removed displaying of password.

--- Argus/Angular-Frontend/src/app/Dashboard/login/login.component.ts
+++ Argus/Angular-Frontend/src/app/Dashboard/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { UserService } from '../../model/user.service';
-import { SessionService } from '../../model/session.service';
-import { User } from '../../model/user';
-import { SessionClass } from '../../model/session';
-import { Session } from '../../../assets/js/SessionStorage.js';
-import { RecoverPasswordEmail } from '../../../assets/js/RecoverPasswordEmail.js';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {UserService} from '../../model/user.service';
+import {SessionService} from '../../model/session.service';
+import {User} from '../../model/user';
+import {SessionClass} from '../../model/session';
+import {Session} from '../../../assets/js/SessionStorage.js';
+import {RecoverPasswordEmail} from '../../../assets/js/RecoverPasswordEmail.js';
 import * as bcrypt from 'bcryptjs';
 // DO NOT REMOVE THIS
 import {forEachComment} from 'tslint';
@@ -23,16 +23,16 @@ export class LoginComponent implements OnInit {
   users: Observable<User[]>;
   sessClass = new SessionClass();
 
-  constructor(private userService: UserService, private sessService: SessionService, private router: Router) { }
+  constructor(private userService: UserService, private sessService: SessionService, private router: Router) {
+  }
 
   recoverPasswordEmail() {
     const emailInp = document.getElementById('emailIn') as HTMLInputElement;
 
-    if (emailInp.value.toLowerCase() !== '' && emailInp.value.toLowerCase() !== 'E-mail'){
+    if (emailInp.value.toLowerCase() !== '' && emailInp.value.toLowerCase() !== 'E-mail') {
       this.sessionS.recoverySess(emailInp.value.toString());
       this.router.navigate(['/reset-password']);
-    }
-    else{
+    } else {
       alert('Error, please enter an email address');
     }
   }
@@ -61,7 +61,7 @@ export class LoginComponent implements OnInit {
           while (data[counter] != null) {
             if (data[counter].userDeleted === null) {
               if (((data[counter].email.toLowerCase() === emailVar.value.toLowerCase()) || (data[counter].username === emailVar.value))
-                && (data[counter].userPass === passVar.value)) {//replace passVar.value with pass
+                && (data[counter].userPass === passVar.value)) { // replace passVar.value with pass
 
                 // tslint:disable-next-line:max-line-length
                 this.sessionS.createSession(data[counter].email, pass, data[counter].userId, data[counter].userRole, data[counter].contactNo);
@@ -76,7 +76,7 @@ export class LoginComponent implements OnInit {
                 this.router.navigate(['/dashboard']);
               }
               if (((data[counter].email.toLowerCase() === emailVar.value.toLowerCase()) || (data[counter].username === emailVar.value)) &&
-                (data[counter].userPass !== passVar.value)) { //replace passVar.value with pass
+                (data[counter].userPass !== passVar.value)) { // replace passVar.value with pass
                 alert('The password you entered seems to be incorrect, please retry entering your password.' + pass);
                 passVar.value = '';
               }
