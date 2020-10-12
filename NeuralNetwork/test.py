b009b7f35303929ec330fa0438d4935561e6f9fa
Bradford-700 <u15228194@tuks.co.za>
2020-10-12 19:00:39 +0200

    travis update

--- NeuralNetwork/test.py
+++ NeuralNetwork/test.py
@@ -1,15 +1,12 @@
 import unittest
 
 from Cam_PI import save_face
-from Cam_PI import classify_face
+from Cam_PI import load_faces
 
 class MyTestCase(unittest.TestCase):
 
     def test_save_face(self):
         self.assertEqual(save_face('models/Jack.npy','data/Jack.jpg'), True)
-
-    def test_classify_face(self):
-        self.assertEqual(classify_face('data/Jack.jpg'),'Jack')
 
 if __name__ == '__main__':
     unittest.main()
