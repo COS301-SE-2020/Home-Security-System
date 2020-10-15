a711fd83a82857d8a860784a37049424578074ea
Bradford-700 <u15228194@tuks.co.za>
2020-10-15 08:11:50 +0200

    Heroku config

--- Spring-Backend/src/main/java/com/springboot/SpringBackend/ArgusDataSource.java
+++ Spring-Backend/src/main/java/com/springboot/SpringBackend/ArgusDataSource.java
@@ -13,9 +13,9 @@ public class ArgusDataSource {
     @Profile("heroku")
     //@LiquibaseDataSource
     public javax.sql.DataSource herokuDataSource() {
-        String username = "vppiqrbkveltmv";
-        String password = "b28603e2c0af7272d8c724ed2be5e8d044896b736d920d9056e55e41ed1b4862";
-        String dbUrl = "jdbc:postgresql://ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/d4pv9m5pfpvjpk";
+        String username = "emgeverppoehii";
+        String password = "b81204231483dbc277a65618de19dcb441089df4bae9864146ed137d90b06f8f";
+        String dbUrl = "jdbc:postgresql://ec2-54-217-206-236.eu-west-1.compute.amazonaws.com:5432/da9s5ch2v5qhrl";
         return DataSourceBuilder.create()
                 .driverClassName("org.postgresql.Driver")
                 .url(dbUrl)
