e34f0805ac3711386465e7047408a40e4e8967e3
Bradford-700 <u15228194@tuks.co.za>
2020-10-15 05:14:44 +0200

    mvn tests

--- Spring-Backend/src/test/java/com/springboot/SpringBackend/SpringBackendApplicationTests.java
+++ Spring-Backend/src/test/java/com/springboot/SpringBackend/SpringBackendApplicationTests.java
@@ -1,8 +1,7 @@
 package com.springboot.SpringBackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- Spring-Backend/src/test/java/com/springboot/SpringBackend/controller/MailerControllerTest.java
+++ Spring-Backend/src/test/java/com/springboot/SpringBackend/controller/MailerControllerTest.java
@@ -3,7 +3,7 @@ package com.springboot.SpringBackend.controller;
 // import com.springboot.SpringBackend.service.MailerService;
 import org.junit.jupiter.api.Test;
 // import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
